726e91961b02dd32030b95fa5b0eff0803d490a7
bastanley1211 <77221444+bastanley1211@users.noreply.github.com>
2021-06-05 11:14:00 -0700

    heroku test

--- src/shared/posts.js
+++ src/shared/posts.js
@@ -26,7 +26,7 @@ export const POSTS = [
   {
     id: 5,
     typeTitle: "A gratitude post by",
-    text: "I am grateful that I can wake up this morning with air in my lungs, drink a warm cup of tea when I wake up, and have an amazing husband.",
+    text: "I am grateful that I can wake up this morning with air in my lungs, drink a warm cup of tea when I wake up, and have an amazing husband. LOLs",
     date: "2018-10-25T16:30Z",
   },
 ];
